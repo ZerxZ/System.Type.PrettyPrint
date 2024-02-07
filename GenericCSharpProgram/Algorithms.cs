87728f01ca2afebaa8808fa8f4991db7290c5d61
深渊の鸽子 <ghzero97@gmail.com>
2024-02-07 03:25:26 +0100

    Add benchmark order and additional type calls

--- GenericCSharpProgram/Algorithms.cs
+++ GenericCSharpProgram/Algorithms.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Order;
 
 namespace GenericCSharpProgram;
 
@@ -9,6 +10,7 @@ namespace GenericCSharpProgram;
     SimpleJob(RuntimeMoniker.Net70, invocationCount: 10000),
     SimpleJob(RuntimeMoniker.Net80, invocationCount: 10000),
 ]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
 [JsonExporterAttribute.Full]
 [JsonExporterAttribute.FullCompressed]
 public partial class Algorithms
@@ -20,6 +22,7 @@ public partial class Algorithms
         call(typeof(List<int>));
         call(typeof(Dictionary<int, int>));
         call(typeof(Action<int, float, double, bool>));
+        call(typeof((List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int, int, int, int, int, int)>)));
     }
 
     private static void ModerateType(Func<Type, string> call)
@@ -29,6 +32,7 @@ public partial class Algorithms
         call(typeof(List<int[,,][,,,]>));
         call(typeof(List<int?[,][,,][,,,]>));
         call((typeof((int, int, int, int, (int, int, int), (int, int), (int, int, int, int, int, int)))));
+        call(typeof((List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List< (int, int, int, int, int, int)>,List<(int,int)>,List< (int, int, int, int, int, int)>)));
     }
 
     private static void CrazyType(Func<Type, string> call)
@@ -38,5 +42,6 @@ public partial class Algorithms
         call(typeof(List<List<List<Dictionary<List<Dictionary<int, IList<Dictionary<int, bool>>>>, int>>>>));
         call(typeof(List<(int, float, (List<Func<List<Dictionary<List<int?>, List<int?[,,]>>>, List<(List<int>, List<int[]>)?>>>, List<int[,,][,][,,,,]>))?>));
         call(typeof((int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int)));
+        call(typeof((List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>,List<(int,int)>)));
     }
 }
